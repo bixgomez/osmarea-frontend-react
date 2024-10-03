972aa6b1a11c85626919436ecaca57a5a47c737e
Richard Gilbert <gilbert67@gmail.com>
2024-10-02 18:06:38 -0700

    Getting rid of old component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import StudyAreasV1 from './StudyAreasV1';
 import StudyAreaDetail from './components/StudyAreaDetail'; // New component for each study area
 
 const App = () => {
@@ -10,9 +9,6 @@ const App = () => {
       <Routes>
         {/* Route for the new main page */}
         <Route path="/" element={<Home />} />
-
-        {/* Route for the old study areas version 1 page */}
-        <Route path="/study-areas--v01" element={<StudyAreasV1 />} />
 
         {/* Dynamic route for each study area using the alias */}
         <Route path="/study-area/:alias" element={<StudyAreaDetail />} />
